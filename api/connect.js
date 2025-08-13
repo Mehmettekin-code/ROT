65a4cf18d8023fe7f90937f2d66d5d44ef949aa3
Mehmettekin-code <mehmetaliercalik03@gmail.com>
2025-08-13 13:51:37 +0300

    Create connect.js

---
+++ api/connect.js
@@ -0,0 +1,14 @@
+// api/connect.js
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB bağlantısı başarılı");
+  } catch (err) {
+    console.error("MongoDB bağlantı hatası:", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
